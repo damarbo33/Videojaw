cad01012e2cf4406886c06b15be6a7800cbf2d0c
Daniel Marco <damarbo33@gmail.com>
2017-08-29 12:59:35 +0200

    A video player with ffmpeg and sdl
    Sources obtained from various places. The intention is to achieve
    knowledge of ffmpeg to do a video editor and port it to android

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW64-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW64-Windows
+CND_ARTIFACT_NAME_Debug=videojaw
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW64-Windows/videojaw
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW64-Windows/package
+CND_PACKAGE_NAME_Debug=videojaw.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW64-Windows/package/videojaw.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW64-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW64-Windows
+CND_ARTIFACT_NAME_Release=videojaw
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW64-Windows/videojaw
+CND_PACKAGE_DIR_Release=dist/Release/MinGW64-Windows/package
+CND_PACKAGE_NAME_Release=videojaw.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW64-Windows/package/videojaw.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
